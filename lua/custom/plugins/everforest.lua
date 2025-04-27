48587243473825fb719b234493ad6c29d2036bfe
Aaron Barnett <aaronbarnett@Aarons-Mac-mini.local>
2025-04-27 12:39:46 -0500

    Add extra configs for everforest

--- lua/custom/plugins/everforest.lua
+++ lua/custom/plugins/everforest.lua
@@ -6,8 +6,12 @@ return {
     config = function()
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
+      vim.opt.background = 'light'
       vim.g.everforest_enable_italic = true
       vim.cmd.colorscheme 'everforest'
+      vim.g.everforest_currend_word = 'underline'
+      vim.g.everforest_background = 'soft'
+      vim.g.everforest_diagnostic_virtual_text = 'colored'
     end,
   },
 }
