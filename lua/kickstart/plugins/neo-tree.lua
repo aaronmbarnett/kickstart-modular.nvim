a8692b31abfe60f7888a88e2a282e5fea0150646
Aaron Barnett <aaronbarnett@Aarons-Mac-mini.local>
2025-04-29 17:02:48 -0500

    Change neotree to follow current file on open

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -9,16 +9,20 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  lazy = false,
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
+  cmd = 'Neotree',
+  -- keys = {
+  --   { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  -- },
   opts = {
     filesystem = {
       window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
+        -- mappings = {
+        --   ['\\'] = 'close_window',
+        -- },
+      },
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = false,
       },
     },
   },
