9a4f0c976b445eefb937d86bcec19096ce1b748d
Aaron Barnett <aaron.barnett@geotoolkit.net>
2025-04-30 10:01:44 -0500

    add modifications for work setup

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -27,6 +27,10 @@ return {
         enabled = true,
         leave_dirs_open = false,
       },
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = false,
+      },
     },
   },
 }
